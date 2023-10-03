03bcdd5ba21914ab2f9a0070023bf5916ada815e
rappakos <rappakos@gmail.com>
2023-10-03 14:03:56 +0200

    glider stats - get counts

--- glider_stats_app/db.py
+++ glider_stats_app/db.py
@@ -17,3 +17,34 @@ async def setup_db(app):
             sql_script = sql_file.read()
             await db.executescript(sql_script)
             await db.commit()
+
+
+async def get_main_counts():
+    year, pilots, flights, gliders = 2023,0,0,0
+    async with aiosqlite.connect(DB_NAME) as db:
+        param = {'year':year}
+        async with db.execute("""SELECT [year], count(pilot_id) [pilots]
+                                 FROM pilots 
+                                 WHERE year = :year 
+                                 GROUP BY [year]""",param) as cursor:
+            async for row in cursor:
+                year = row[0]
+                pilots = row[1]
+        async with db.execute("""SELECT count(f.flight_id) [flights]
+                                 FROM pilots p
+                                 INNER JOIN flights f ON f.pilot_id=p.pilot_id
+                                 WHERE p.year = :year 
+                                 GROUP BY [year]""",param) as cursor:
+            async for row in cursor:
+                flights = row[0]
+
+        async with db.execute("""SELECT count(distinct f.glider) [gliders]
+                                 FROM pilots p
+                                 INNER JOIN flights f ON f.pilot_id=p.pilot_id
+                                 WHERE p.year = :year 
+                                 GROUP BY [year]""",param) as cursor:
+            async for row in cursor:
+                gliders = row[0]
+
+
+    return  year, pilots, flights, gliders  
--- glider_stats_app/views.py
+++ glider_stats_app/views.py
@@ -2,6 +2,15 @@
 import aiohttp_jinja2
 from aiohttp import web
 
+from . import db
+
 @aiohttp_jinja2.template('index.html')
 async def index(request):
-    return {'res':'test'}
+    year, pilots, flights, gliders = await db.get_main_counts()
+
+    return {
+            'year':year,
+            'pilots': pilots,
+            'flights': flights,
+            'gliders': gliders
+    }
